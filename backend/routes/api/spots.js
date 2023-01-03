91f8b324c1db914e065ae2f2ec224337898ffd80
Shibo <hushibo456@gmail.com>
2023-01-02 21:52:45 -0500

    1

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -16,7 +16,7 @@ const validateSpot = [
     check("country")
       .exists({ checkFalsy: true })
       .withMessage("Country is required"),
-    check("lat").exists({ checkFalsy: true }),
+    check("lat").exists({ checkFalsy: true }).withMessage('Latitude is not valid'),
     check("lng")
       .exists({ checkFalsy: true })
       .withMessage("Longitude is not valid"),
