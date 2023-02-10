2ffcd39c02ad6e18f0b98dc6345d175b53c601a0
Shibo <hushibo456@gmail.com>
2023-02-10 09:15:29 -0500

    render

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import './index.css';
+import ReactDOM from 'react-dom';
+import configureStore from './store';
+import * as sessionActions from './store/session';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import configureStore from './store';
-import { restoreCSRF, csrfFetch } from './store/csrf';
-import * as sessionActions from './store/session';
+import { ModalProvider, Modal } from './context/Modal';
+import { restoreCSRF, csrfFetch } from "./store/csrf";
+
 
 const store = configureStore();
 
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== "production") {
   restoreCSRF();
 
   window.csrfFetch = csrfFetch;
@@ -20,11 +22,14 @@ if (process.env.NODE_ENV !== 'production') {
 
 function Root() {
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+    <ModalProvider>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+          <Modal />
+        </BrowserRouter>
+      </Provider>
+    </ModalProvider>
   );
 }
 
