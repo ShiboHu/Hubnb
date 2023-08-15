4ee8ffb7c5b0d7a9d219a7c9d562378344d3b3f8
shibohu <hushibo456@gmail.com>
2023-08-14 21:41:49 -0400

    c

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -15,6 +15,7 @@ if (process.env.NODE_ENV === 'production') {
       path.resolve(__dirname, '../../frontend', 'build', 'index.html')
     );
   });
+  
 
   // Serve the static assets in the frontend's build folder
   router.use(express.static(path.resolve("../frontend/build")));
